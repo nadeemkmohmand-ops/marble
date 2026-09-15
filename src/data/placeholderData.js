6bbe3b51f875a9cf79f554baeda7361a50631326
nadeemkmohmand-ops <nadeemk.mohmand@gmail.com>
2026-09-14 22:05:15 -0700

    Refactor exports from app.js for better structure

--- src/data/placeholderData.js
+++ src/data/placeholderData.js
@@ -8,4 +8,10 @@ export { homeStats, quickActions, recentActivities } from './home.js'
 export { weeklyProduction, monthlyProduction, slabTypes, reportSummary } from './reports.js'
 export { inventoryItems, sizeOptions, thicknessOptions, locationOptions } from './inventory.js'
 export { pieceSizeOptions, calcHistory } from './calculator.js'
-export { appFeatures, factoryInfo, featureIcon } from './app.js'
+export {
+  appFeatures,
+  factoryInfo,
+  managementTeam,
+  factoryDesignations,
+  featureIcon,
+} from './app.js'
